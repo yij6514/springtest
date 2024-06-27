8b46071b518660d4d44eb7ce87f83437b4cd006c
yij6514 <yij6514@gmail.com>
2024-06-27 18:12:46 +0900

    using crudrepository

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -27,7 +31,12 @@ public class DemoApplication {
 
 }
 
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
+//h2를 위한 jpa의 어노테이션
+@Entity
 class Coffee{
+	@Id
 	private String id;
 	private String name;
 	
@@ -65,62 +74,52 @@ class Coffee{
 //localhost:8080/coffee 로 요청하는 http 요청을 수행
 @RequestMapping("/coffees")
 class RestApiDemoController{
-	private List<Coffee> coffees = new ArrayList<>();
+	//arraylist 대신에 사용되는 사용되는 객체
+	private final CoffeeRepository coffeeRepository;
 	
-	public RestApiDemoController() {
-		coffees.addAll(List.of(
-					new Coffee("Cafe Cereza"),
-					new Coffee("Cafe Ganador"),
-					new Coffee("Cafe Lareno"),
-					new Coffee("Cafe Tres Pontas")
+	//기본적인 생성자
+	public RestApiDemoController(CoffeeRepository coffeeRepository) {
+		this.coffeeRepository = coffeeRepository;
+		
+		//crudRepository에 saveall 메소드는 일련의 리스트를 사용한다.
+		this.coffeeRepository.saveAll(List.of(
+				new Coffee("Cafe Cereza"),
+				new Coffee("Cafe Ganador"),
+				new Coffee("Cafe Lareno"),
+				new Coffee("Cafe Tres Pontas")
 				));
 	}
 	
 	//HTTP Get 메소드로 localhost:8080/coffees 로 리스트에 있는 커피를 모두 보여줌
 	@GetMapping
 	Iterable<Coffee> getCoffees() {
-		return coffees;
+		//findall 메소드는 안에 들어있는 객체를 전부 반환
+		return coffeeRepository.findAll();
 	}
 	
 	//HTTP Get 메소드로 localhost:8080/coffees/id 로 리스트 중에 있는 특정 id의 커피를 보여줌
 	@GetMapping("/{id}")
 	Optional<Coffee> getCoffeeById(@PathVariable String id) {
-		for(Coffee c: coffees) {
-			if(c.getId().equals(id)) {
-				return Optional.of(c);
-			}
-		}
-		
-		return Optional.empty();
+		return coffeeRepository.findById(id);
 	}
 	
 	//HTTP Post 메소드로 localhost:8080/coffees 로 받을때 json에 있는 정보로 커피를 추가함
 	@PostMapping
 	Coffee postCoffee(@RequestBody Coffee coffee) {
-		coffees.add(coffee);
-		return coffee;
+		return coffeeRepository.save(coffee);
 	}
 	
 	//HTTP Put 메소드로 localhost:8080/coffees/id 로 받을때 json에 있는 정보로 만약 coffees 리스트에 없으면 return 함수로 추가 있다면 수정함
 	@PutMapping("/{id}")
 	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-		int coffeeIndex = -1;
-		
-		for(Coffee c: coffees) {
-			if(c.getId().equals(id)) {
-				coffeeIndex = coffees.indexOf(c);
-				coffees.set(coffeeIndex, coffee);
-			}
-		}
-		
-		return (coffeeIndex == -1) ? 
-				new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-				new ResponseEntity<>(coffee, HttpStatus.OK);
+		return (!coffeeRepository.existsById(id))
+				? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
+				: new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
 	}
 	
 	//HTTP Post 메소드로 localhost:8080/coffees/id 로 받을때 id의 정보로 커피를 삭제함
 	@DeleteMapping("/{id}")
 	void deleteCoffee(@PathVariable String id) {
-		coffees.removeIf(c -> c.getId().equals(id));
+		coffeeRepository.deleteById(id);
 	}
 }
